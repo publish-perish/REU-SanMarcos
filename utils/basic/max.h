d4e9b85e9fe07de69a9ed8da2e027d2258d1f2c9
JordanBlocher <jordanblocher@gmail.com>
2012-06-21 11:48:24 -0500

    Basic file tree. Mostly empty.

---
+++ utils/basic/max.h
@@ -0,0 +1,17 @@
+#ifndef H_H
+#define H_H
+
+#include <tr1/tuple>
+
+class max{
+
+   public:
+
+      double m_best;
+      double M[];
+
+}
+
+
+
+#endif
---
+++ utils/math/max.cpp
@@ -0,0 +1,13 @@
+#include "../basic/max.h"
+
+
+max::max()
+{
+}
+
+max::max(tuple A, tuple Q)
+{
+
+   return 0;
+
+}
