cfa99945b293cfaae9af2763e3dc3e850b59808b
JordanBlocher <jordanblocher@gmail.com>
2012-06-23 18:15:09 -0500

    Starting to code (finally).

--- utils/math/tuple.cpp
+++ utils/math/tuple.cpp
@@ -4,6 +4,23 @@ template<typename TP, int N>
 Tuple Tuple::operator-(const Tuple &t)
 {
    double a, b, c; // temp variables for polynomial ax + bx^2 + cx^3
+   bool flag = true;
+   vector<TP> list;
 
+   for(int i; i<sizeof(t); i++)
+   {
+      if( data.at(i) < t.data.at(i))
+      {
+         flag = false;
+      }
+      else
+      {
+         list.push_back(data.at(i)-t.data.at(i));
+      }
+
+   }
+   if(flag)
+   {
+      return Tuple<TP, N>(list);
+   }
 }
-
