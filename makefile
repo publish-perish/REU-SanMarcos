0be30d9eb0351cdc1fd4b16f2e919c94ba95db91
JordanBlocher <jordanblocher@gmail.com>
2012-06-25 10:33:08 -0500

    Semi-working example.

--- makefile
+++ makefile
@@ -2,21 +2,20 @@ NC=-I/usr/local/include
 INC= -g
 LIBS=
 
-test: test.o polynomials.o permutations.o tuple.o
-	g++ $(INC) -o executables/test $(LIBS) ./executables/test.o ./executables/polynomials.o ./executables/permutations.o ./executables/tuple.o
-average_case: driver.o polynomials.o permutations.o tuple.o
-	g++ $(INC) -o executables/average_case $(LIBS) ./executables/driver.o ./executables/polynomials.o ./executables/permutations.o ./executables/tuple.o
+average_case: driver.o polynomials.o permutations.o subtraction.o
+	g++ $(INC) -o executables/average_case $(LIBS) ./executables/driver.o ./executables/polynomials.o ./executables/permutations.o ./executables/subtraction.o
+test: polynomials.o permutations.o test.o subtraction.o
+	g++ $(INC) -o executables/test $(LIBS) ./executables/test.o ./executables/polynomials.o ./executables/permutations.o ./executables/subtraction.o
 driver.o: ./src/driver.cpp
 	g++ $(LIB) $(INC) -c -o executables/driver.o ./src/driver.cpp
 polynomials.o: ./utils/math/polynomials.cpp
 	g++ $(LIB) $(INC) -c -o executables/polynomials.o ./utils/math/polynomials.cpp
 permutations.o: ./utils/math/permutations.cpp
 	g++ $(LIB) $(INC) -c -o executables/permutations.o ./utils/math/permutations.cpp
-tuple.o: ./utils/math/tuple.cpp
-	g++ $(LIB) $(INC) -c -o executables/tuple.o ./utils/math/tuple.cpp
-test.o: ./src/test.cpp
-	g++ $(LIB) $(INC) -c -o executables/test.o ./src/test.cpp
-
+subtraction.o: ./utils/math/subtraction.cpp
+	g++ $(LIB) $(INC) -c -o executables/subtraction.o ./utils/math/subtraction.cpp
+test.o: ./test/test.cpp
+	g++ $(LIB) $(INC) -c -o executables/test.o ./test/test.cpp
 clean:
 	rm executables/*
 
--- utils/math/polynomials.cpp
+++ utils/math/polynomials.cpp
@@ -4,13 +4,19 @@
 
 Polynomial::Polynomial()
 {
-   A = Y  = T(0,0,0);
 }
 
-Polynomial::Polynomial(T &a, T &y)
+Polynomial::Polynomial(T a, T y)
 {
    A = a;
    Y = y;
+   s = 0;
+}
+
+Polynomial::Polynomial(const Polynomial &p)
+{
+    A = p.A;
+    Y = p.Y;
 }
 
 T Polynomial::value()const
@@ -20,7 +26,88 @@ T Polynomial::value()const
 
 int Polynomial::sum()const
 {
-return((get<0>(A)*get<0>(Y)) + (get<1>(A)*get<1>(Y)) +(get<2>(A)*get<2>(Y)));
+   return((get<0>(A)*get<0>(Y)) + (get<1>(A)*get<1>(Y)) +(get<2>(A)*get<2>(Y)));
 }
+
+bool Polynomial::operator==(const Polynomial &p)
+{
+   return((get<0>(A)==get<0>(Y)) && (get<1>(A)==get<1>(Y)) && (get<2>(A)==get<2>(Y)));
+}
+
+Polynomial Polynomial::operator=(Polynomial p)
+{
+    Polynomial temp(p.A, p.Y);
+    temp.s = p.s;
+    return temp;
+}
+
+void Polynomial::borrowC(Polynomial &p)
+{
+   std::cout<<p;
+   if( get<1>(p.Y) < 0 )
+   {
+         ++p.s.c_borrowed;
+         Polynomial temp(p.A, T(get<0>(p.Y), get<1>(p.Y)+get<0>(p.A), get<2>(p.Y)));
+         p = temp;
+         (borrowC(temp));
+   }
+         std::cout<<"Returned to borrowC "<<p;
+}
+
+void Polynomial::borrowB(Polynomial &p)
+{
+    std::cout<<p;
+    if( get<2>(p.Y) < 0 )
+    {
+         ++p.s.b_borrowed;
+         Polynomial temp(p.A, T(get<0>(p.Y), get<1>(p.Y), get<2>(p.Y)+get<1>(p.A)));
+         p = temp;
+         (borrowB(temp));
+    }
+    else if( get<1>(p.Y) < 0 )
+    {
+        std::cout<<"Borrowing from C \n";
+        borrowC(p);
+    }
+        std::cout<<"Returned to borrowB "<<p;
+}
+
+
+Polynomial Polynomial::operator-(Polynomial m)
+{
+    Polynomial value = *this;
+    std::cout<<"Testing: \n"<<"   "<<*this<<" - "<<m<<std::endl;
+
+    if( (get<0>(Y) > get<0>(m.Y)) )
+    {
+        ++m.s.m_subtracted;
+        Polynomial temp(A, T(get<0>(Y) - get<0>(m.Y), get<1>(Y) - get<1>(m.Y), get<2>(Y) - get<2>(m.Y)));
+        value = temp;
+        temp = temp - m;
+        std::cout<< "Subtracted "<< m.s.m_subtracted<<" times. \n";
+        std::cout<< temp<<std::endl;
+    }
+    else if( get<2>(Y) < 0 )
+    {
+          std::cout<<"Borrowing from B \n";
+          borrowB(value);
+          std::cout<<"Returned to main subtract "<<value;
+    }
+    std::cout<<"end subtract \n";
+    return value;
+}
+
+std::ostream& operator<<(std::ostream& ostr, const Polynomial &p)
+{
+    ostr << get<2>(p.Y) << "a + " << get<1>(p.Y) << "b + " << get<0>(p.Y) <<"c \n";
+    return ostr;
+}
+
+std::ofstream& operator<<(std::ofstream& ofstr, const Polynomial &p)
+{
+    ofstr << get<2>(p.Y) << "a + " << get<1>(p.Y) << "b + " << get<0>(p.Y) <<"c \n";
+    return ofstr;
+}
+
 
 
