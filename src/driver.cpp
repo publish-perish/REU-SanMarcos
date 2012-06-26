ccc20d1e5605dd840d8fadf7ba5112d56f7ca60e
christopher-linden <lindenechris@gmail.com>
2012-06-26 12:21:12 -0500

    remade

--- src/driver.cpp
+++ src/driver.cpp
@@ -39,14 +39,6 @@ C.makeMcoTable(10); //takes an arbitrary? bound
 G.makeGenTable(10); //arbitrary? bound
 std::cout <<"\n cotSize:" << C.getCotSize() << "\n mtSize:" << C.getMtSize() << "\n gSize:" << G.getGsize() << endl;
 return 0;
-#include "../utils/basic/subtraction.h"
-
-int main()
-{
-T A = T(2,12,12);
-T B = T(0,3,2);
-A.subtract(B);
-
 }
 */
 ///*
@@ -54,8 +46,8 @@ int main()
 {
 const int diam = 10; //make sure this matches the diam used to generate the tables
 const double lowerbound = (1000 / 16); // diam cubed over 16
-Subtractions best[xSize]; //holds the xcos table's size many subtractions: gives the complete history
-Subtractions temp[xSize];
+Polynomial best[xSize]; //holds the xcos table's size many polynomial: gives the history
+Polynomial temp[xSize];
 bool cover[diamCubed]; // diam cubed: larger than needed, but hard to make sharp
 bool covered =false;
 int counter = 0; //index for the temp array
@@ -67,6 +59,7 @@ T Q; //m coefs
 T x; //x coefs
 Polynomial X;
 Polynomial M; //the bound itself
+Polynomial Adj;
 int m; //holds sum of M
 int mbest = 0; //holds the highest valid m
 gens.open("./permutationtables/gentable.txt");
@@ -84,10 +77,10 @@ if(gens)
 				mcos >> boost::tuples::set_open('(') >> boost::tuples::set_close(')') >> boost::tuples::set_delimiter(',') >> Q;
 				M = Polynomial(A, Q);
 				m = M.sum();
-				cout <<  m << endl;;
+				//cout <<  m << endl;;
 				memset(cover,false,diamCubed);
-				cout << "memset success" << endl;
-				if( m >= mbest)
+				//cout << "memset success" << endl;
+				if( m >= mbest) //ignore M that are too small, or badly formed. need a well formed function from polynomial.cpp?
 				{
 					xcos.open("./permutationtables/cotable.txt");
 					counter = 0;
@@ -95,23 +88,25 @@ if(gens)
 					{
 							while(!xcos.eof())
 							{
-							cout << "in the while loop" << endl;
-								xcos >> boost::tuples::set_open('(') >> boost::tuples::set_close(')') >> boost::tuples::set_delimiter(',') >> x;
-							cout<< "read in"  << endl;
-								//cout << x << endl;
+							//cout << "in the while loop" << endl;
+							xcos >> boost::tuples::set_open('(') >> boost::tuples::set_close(')') >> boost::tuples::set_delimiter(',') >> x;
+							//cout<< "read in"  << endl;
+							//	cout << x << endl;
 								X = Polynomial(A, x);
-							cout << "assigned poly" << endl;
-							temp[counter].clear();
-								subtract(X, M, temp[counter]);
-							cout << "subtraction done" << endl;
-								cover[X.sum()] = true;							
+								cout << "assigned poly" << A << Q << x << endl;
+							//	temp[counter].clear();
+								cout << "cleared" << endl;
+								cout << X << endl << M <<endl;
+								Adj = X-M;
+								cout << "count" << counter << endl;
+								temp[counter] = Adj;
+								cout << "subtraction done" << endl;
+								cover[Adj.sum()] = true;							
 								++counter;
 							}
-						cout << "out of the while" << endl;
-								
-						
+						//cout << "out of the while" << endl;
 						xcos.close();
-						cout << "xcos closed" << endl;
+					//	cout << "xcos closed" << endl;
 						covered = true;
 						for(int i=0; i < m; ++i) //only check the first m of them
 						{
@@ -128,7 +123,7 @@ if(gens)
 								{
 									best[j] = temp[j];
 								}
-							cout << m << endl << A  << endl;
+						//	cout << m << endl << A  << endl;
 						}
 					}
 				}
@@ -139,7 +134,19 @@ if(gens)
 	}
 gens.close();
 }
-	
+//need to write out mbest and best to a file.	
 return 0;
 }
 //*/
+/*
+int main()
+{
+T gens = T(3, 2, 1);
+T one = T(1, 0 ,0);
+T zero = T(0,0,1);
+Polynomial A(gens, one);
+Polynomial B(gens, zero);
+Polynomial C;
+C = A-B;
+}
+*/
