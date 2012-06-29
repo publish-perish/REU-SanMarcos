91124712a912ad2305d7363ac9f9eb8c71dc276b
JordanBlocher <jordanblocher@gmail.com>
2012-06-29 16:58:42 -0500

    Single input.

--- src/tables.cpp
+++ src/tables.cpp
@@ -10,8 +10,8 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     clock_t start, end;
-    if(argc<4){
-       std::cout<< "Usage: ./executables/tables int int int \n";
+    if(argc<2){
+       std::cout<< "Usage: ./executables/tables diameter \n";
        return 0;
     }
     else{
@@ -19,10 +19,10 @@ int main(int argc, char *argv[])
        CoefTable C;
        GenTable G;
        //std::cout << "Coefficients:";
-       C.makeCoTable(atoi(argv[1])); // takes diameter
-       C.makeMcoTable(atoi(argv[2])); //takes an arbitrary? bound
+       C.makeCoTable(atoi(argv[1]));
+       C.makeMcoTable(atoi(argv[1]));
        //std::cout << "\n Generators: \n";
-       G.makeGenTable(atoi(argv[3])); //arbitrary? bound
+       G.makeGenTable(atoi(argv[1]));
        end = clock();
        std::cout<<"Tables were generated in "<<(double)(end - start)/(double)CLOCKS_PER_SEC<<" seconds.\n";
        std::cout <<"\n cotSize:" << C.getCotSize() << "\n mtSize:" << C.getMtSize() << "\n gSize:" << G.getGsize() << endl;
